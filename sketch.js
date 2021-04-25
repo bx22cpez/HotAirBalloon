85584074bfe9910e1cce26067f56ef0ea466b315
bx22cpez <76233353+bx22cpez@users.noreply.github.com>
2021-04-25 12:57:37 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,46 +1,81 @@
-var ball, database;
+var balloon,balloonImage1,balloonImage2;
 
+var database, position;
+// create database and position variable here
 
-function setup(){
-    createCanvas(500,500);
-    database = firebase.database();
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-    var ballposition = database.ref("ball/position");
-    ballposition.on("value",readPosition,showError);
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+  var balloonposition = database.ref("ball/height");
+  balloonposition.on("value",readPosition,showError);
+
+  textSize(20); 
 }
 
-function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+  }
+
+
+  if (keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale = balloon.scale -0.01;
+  }
+
+
+
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
 }
 
-function changePosition(x,y){
-    database.ref('ball/position').set({
-        'x': position.x + x,
-        'y': position.y + y
-    })
+function updateHeight(x,y){
+  database.ref('ballon/height').set({
+    'x': height.x + x ,
+    'y': height.y + y
+  })
 }
-//.ref() is used to refer to the location of the database value we care about. .on() creates a listener which keeps listening to the changes in the database.
-//.set() is used to set the value in the database.
-function readPosition (data){
-    position = data.val();
-    ball.x = position.x;
-    ball.y = position.y;
+
+function readHeight(data){
+  height = data.val();
+  balloon.x = height.x;
+  balloon.y = height.y;
 }
 
 function showError(){
-    console.log("error");
+  console.log("Error in writing to the database");
 }
